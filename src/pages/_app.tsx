979ec4ae5387c7c5d6d419d786655dfbe9826854
FIREPC\FIRE <firebenderyt@gmail.com>
2023-05-25 02:06:14 +0300

    site init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const AnyComponent = Component as any;
+  return <AnyComponent {...pageProps} />;
+}
---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  namespace JSX {
+    // eslint-disable-next-line no-unused-vars
+    interface IntrinsicElements {
+      [elemName: string]: any;
+    }
+  }
+}
